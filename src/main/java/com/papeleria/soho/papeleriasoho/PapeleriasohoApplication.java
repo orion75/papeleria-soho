acb23a6b483f2d0af11f6279ad4f59446f9aa33b
Harol Alfonso Reina H <hreina@tuprimero.com>
2022-10-10 06:51:22 -0500

    first commit

---
+++ src/main/java/com/papeleria/soho/papeleriasoho/PapeleriasohoApplication.java
@@ -0,0 +1,13 @@
+package com.papeleria.soho.papeleriasoho;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PapeleriasohoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PapeleriasohoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/papeleria/soho/papeleriasoho/PapeleriasohoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.papeleria.soho.papeleriasoho;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PapeleriasohoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

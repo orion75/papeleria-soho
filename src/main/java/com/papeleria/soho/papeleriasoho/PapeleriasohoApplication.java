a15b8097690728f421ada4a4056e56be3cf7c92c
Harol Alfonso Reina H <hreina@tuprimero.com>
2022-10-16 12:50:53 -0500

    feat: configuracion aplicacionswagger modelmapper

--- src/main/java/com/papeleria/soho/papeleriasoho/PapeleriasohoApplication.java
+++ src/main/java/com/papeleria/soho/papeleriasoho/PapeleriasohoApplication.java
@@ -1,13 +1,37 @@
 package com.papeleria.soho.papeleriasoho;
 
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+
+import org.modelmapper.ModelMapper;
+import org.modelmapper.convention.MatchingStrategies;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class PapeleriasohoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(PapeleriasohoApplication.class, args);
+	}
+
+	@Bean
+	public OpenAPI customOpenAPI(@Value("${springdoc.version}") String appVersion) {
+		return new OpenAPI()
+				.components(new Components())
+				.info(new Info().title("Papeleria Soho API").version(appVersion)
+						.license(new License().name("Apache 2.0").url("http://springdoc.org")));
+	}
+
+	@Bean
+	public ModelMapper modelMapper(){
+		ModelMapper modelMapper = new ModelMapper();
+		modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
+		return modelMapper;
 	}
 
 }

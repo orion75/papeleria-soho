ada1ae14c8092189eec29117ef54c98a88eb7615
Harol Alfonso Reina H <hreina@tuprimero.com>
2022-10-16 05:52:40 -0500

    feat: login model

---
+++ src/main/java/com/papeleria/soho/papeleriasoho/payload/request/LoginRequest.java
@@ -0,0 +1,17 @@
+package com.papeleria.soho.papeleriasoho.payload.request;
+
+import javax.validation.constraints.NotBlank;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class LoginRequest {
+    
+    @NotBlank
+    private String username;
+
+    @NotBlank
+    private String password;
+}
